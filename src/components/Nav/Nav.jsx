4c30e7da0f8b0639d4ecbd0c46e6d38d66ed67a3
khalidhersi <cipherpolfoundation@gmail.com>
2022-03-25 00:40:54 +0000

    nav complete

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./Nav.scss";
 
 const Nav = () => {
   return (
-    <div className='nav'>
-        <h1 className="nav__text">
-            WORDLE
-        </h1>
+    <div className="nav">
+      <div className="nav__container">
+        <div className="nav__container__left">
+          <i class="fa-solid fa-circle-question nav__container__icon"></i>
+        </div>
+        <div className="nav__container__center">
+          <h1 className="nav__container__text">WORDLE</h1>
+        </div>
+        <div className="nav__container__right">
+          <i class="fa-solid fa-chart-simple nav__container__icon"></i>
+          <i class="fa-solid fa-gear nav__container__icon"></i>
+        </div>
+      </div>
+      <div className="nav__underline"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
