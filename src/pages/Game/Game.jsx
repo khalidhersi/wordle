9af90d5fcb716515136db8b3a364790105ad56d1
khalidhersi <cipherpolfoundation@gmail.com>
2022-03-25 11:50:01 +0000

    changed tile comp for a empty, changed col to row along with flex direction

--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,52 +1,54 @@
-import React from 'react'
-import Tile from '../../components/Tile/Tile';
+import React from "react";
+import Tile from "../../components/Tile/Tile";
 import "./Game.scss";
 
 const Game = () => {
   return (
-    <div className='gameboard'>
-        <div className="gameboard__col">
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            </div>
-        <div className="gameboard__col">
-            <Tile className="gameboard__tile--correct-place"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            </div>
-        <div className="gameboard__col">
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            </div>
-        <div className="gameboard__col">
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            </div>
-        <div className="gameboard__col">
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            <Tile className="gameboard__tile"/>
-            </div>
+    <div className="gameboard">
+      <div className="gameboard__row">
+        <div className="gameboard__tile">A</div>
+        <div className="gameboard__tile --correct-place">B</div>
+        <div className="gameboard__tile --incorrect-letter">F</div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
+      <div className="gameboard__row">
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile --correct-letter">E</div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
+      <div className="gameboard__row">
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
+      <div className="gameboard__row">
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
+      <div className="gameboard__row">
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
+      <div className="gameboard__row">
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+        <div className="gameboard__tile"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
